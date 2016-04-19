c632ff328a947b587b13800575bcf2a3b7b8079e
Corwin Hansen <Corwin@Corwins-Mac-Pro.local>
2016-04-18 17:42:09 -0700

    Added Starting files

---
+++ Main.c
@@ -0,0 +1,21 @@
+//Copyright (C) 2016  Corwin Hansen
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+//find a way to easily silmulate 1 and 2 dice with 3
+//for correct result for 2 dice simulation, doubles should get 6 and the rest should get 12
+
+
+
+
+int main(int argc, char *argv[]){
+
+}
+
+
+
+
+
+
